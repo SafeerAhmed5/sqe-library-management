40d1612780862f25ceceb712c8af89cae865ae3d
Safeer Ahmed <safeerahmedmirani4@gmail.com>
2026-09-01 03:04:12 -0700

    feat(library): validate duplicate ISBN

---
+++ src/Library.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+
+    private List<Book> books = new ArrayList<>();
+
+    public void addBook(Book book) {
+    for (Book existingBook : books) {
+        if (existingBook.getIsbn().equals(book.getIsbn())) {
+            throw new IllegalArgumentException("Book with this ISBN already exists");
+        }
+    }
+
+    books.add(book);
+}
+
+}
