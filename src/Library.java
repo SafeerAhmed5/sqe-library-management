eea44ce89cb4544229ba81904036caf8917af64c
Safeer Ahmed <safeerahmedmirani4@gmail.com>
2026-09-01 03:05:50 -0700

    docs(library): document addBook method

--- src/Library.java
+++ src/Library.java
@@ -6,6 +6,7 @@ public class Library {
     private List<Book> books = new ArrayList<>();
 
     public void addBook(Book book) {
+    public void addBook(Book book) {
     for (Book existingBook : books) {
         if (existingBook.getIsbn().equals(book.getIsbn())) {
             throw new IllegalArgumentException("Book with this ISBN already exists");
