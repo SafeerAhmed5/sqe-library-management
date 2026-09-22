2991879413f232a4b63bffc1a572192e756594e0
Safeer Ahmed <safeerahmedmirani4@gmail.com>
2026-09-22 03:02:02 -0700

    test: add equivalence partitioning tests for GradeBook

---
+++ tests/test_roster.py
@@ -0,0 +1,24 @@
+import pytest
+
+from gradebook.gradebook import Roster
+
+
+class Student:
+    def __init__(self, scores):
+        self.scores = scores
+
+
+@pytest.mark.parametrize('score_count,expected_error', [
+    (0, True),
+    (3, False),
+    (8, True),
+])
+def test_score_count_classes(score_count, expected_error):
+    roster = Roster()
+    student = Student([80] * score_count)
+
+    if expected_error:
+        with pytest.raises(ValueError):
+            roster.add_student(student)
+    else:
+        roster.add_student(student)
